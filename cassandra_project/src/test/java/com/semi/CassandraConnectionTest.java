826e867107f397a9e5e5cb04d5e1c7c28a0dfae7
Sulaymon <demirel6777@gmail.com>
2019-11-14 14:26:22 +0300

    test connection with cassandra

---
+++ cassandra_project/src/test/java/com/semi/CassandraConnectionTest.java
@@ -0,0 +1,38 @@
+package com.semi;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.Objects;
+
+
+public class CassandraConnectionTest {
+    private Cluster cluster;
+    private Session session;
+
+    @Test
+    public void connectToNode () {
+        String NODE = "127.0.0.1";
+        int port = 9042;
+        connect(NODE, port);
+    }
+
+    private void connect(String node, Integer port) {
+        Cluster.Builder builder = Cluster.builder().addContactPoint(node);
+        if (Objects.nonNull(port)) {
+            builder.withPort(port);
+        }
+
+        cluster = builder.build();
+        session = cluster.connect();
+//        session.execute("select * from test.test");
+    }
+
+    @After
+    public void close () {
+        session.close();
+        cluster.close();
+    }
+}
